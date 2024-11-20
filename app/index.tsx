9ec18f822efcfb60f3539b3b25b5ec859c77cc51
SIU <90703662+NguyenNhatMinh-BYS@users.noreply.github.com>
2024-11-21 00:15:46 +0700

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput defaultValue="123"/>
+    </View>
+  );
+}
